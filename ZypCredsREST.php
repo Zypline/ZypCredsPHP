819a75013b82d71875f0b5faf3fb52e9d1bc579e
Greg K <Greg@ghlk.net>
2012-09-09 22:17:54 -0700

    Updated version - improved comments and descriptions
    Mostly whitespace edits, also reformatted some code and comment blocks
    as well as added a comment block to parse_data() and added a summary to
    parse_json()

--- ZypCredsREST.php
+++ ZypCredsREST.php
@@ -2,7 +2,7 @@
 /**
  * Class ZypCredsREST
  * Summary: A libary which handles requests for ZypCreds REST API. 
- * Version: 0.5 pre-release
+ * Version: 0.6
  * Authors: Greg Kasbarian
  * Initial Release Date: n/a
  *
@@ -27,8 +27,10 @@
 class ZypCredsREST{
 	
 	# Your API Credentials
-	private $api_id  = '';
-	private $api_key = '';
+	private $api_id  = ''; # Place your api ID  here <---
+	private $api_key = ''; # Place your api KEY here <---
+
+	# ---
 
 	# URIs
 	private $verify_uri		= 'http://api.zypcreds.com/verify/';
@@ -53,17 +55,16 @@ class ZypCredsREST{
 
 
 	/**
-	 * __construct( $api_id, $api_key )
-	 * Summary: Constructor requires $api_id and $api_key to complete. If both passed, places values in class variables.
-	 * @param 	str 	$api_id 	User's API ID
-	 * @param 	str 	$api_key 	User's API KEY
+	 * __construct()
+	 * Summary: Requires $api_id and $api_key to begin.
+	 * @param 	str 	$api_id 	Your API ID
+	 * @param 	str 	$api_key 	Your API KEY
 	 * @return 	bool 	True 		Constructor successful.
 	 * @return 	bool 	False 		Constructor failed.
 	 **/
 	function __construct( $api_id, $api_key ){
 
 		if( !$api_id || !$api_key ){
-			$this->error_desc = "Api ID and Api KEY are both required.";
 			return false;
 		}else{
 			$this->api_id = $api_id;
@@ -78,11 +79,12 @@ class ZypCredsREST{
 	# -------------------------
 
 	/**
-	 * request_verification( $index )
-	 * Summary: Request a verification code be sent to the $index provided.
-	 * @param 	str 	$index 	Index to request verification for.
-	 * @return 	bool 	False 	Error occurred.
-	 * @return 	obj 	$data 	Data returned from ->call_uri() call.
+	 * request_verification()
+	 * Summary: Requests a verification code be sent to the index provided.
+	 *	Preps parameters, makes curl call, and then parses data into class variables.
+	 * @param 	str 	$index 	Index requesting verification for.
+	 * @return 	bool 	False 	Error occurred - error number and description set.
+	 * @return 	obj 	$data 	Raw-content returned from ->call_uri() call.
 	 **/
 	function request_verification( $index ){
 
@@ -107,7 +109,7 @@ class ZypCredsREST{
 
 
 	/**
-	 * attempt_verification( $index, $code )
+	 * attempt_verification()
 	 * Summary: Check if $code provided is a valid verification code for the $index provided.
 	 * @param 	str 	$index 		The index you wish to verify.
 	 * @param 	str 	$code 		Code entered by user.
@@ -313,6 +315,12 @@ class ZypCredsREST{
 	}
 
 
+	/**
+	 * parse_data()
+	 * Summary: Calls either JSON or XML parse depending on what the accepted content type is.
+	 * @param 	str 	$data 	Returned content from the ->call_uri() call.
+	 * @return 	void 
+	 **/
 	private function parse_data( $data ){
 		
 		if( $this->accept_type == 'json' ){
@@ -331,30 +339,28 @@ class ZypCredsREST{
 	 **/
 	private function parse_xml( $xml ){
 
-		if( !$xml ){
-			return false;
-		}
+		if(!$xml){ return false; }
+
 		$obj = new SimpleXMLElement( $xml );
-		
 		# Use (string) or (int) or SimpleXMLElement passes an object and causes SESSION fail.
 		$this->prepped_index = 	(string)$obj->index['prepped'];
 		$this->result_bool = 	(string)$obj->result['bool'];
 		$this->result_desc = 	(string)$obj->result['description'];
 		$this->error_no = 		(string)$obj->error['number'];
 		$this->error_desc = 	(string)$obj->error['description'];
-
 	}
 
 
 	/**
 	 * parse_json
-	 * Summary:
+	 * Summary: Parses JSON and places values in class variables.
 	 * @param 	str 	$json 	The resulting JSON content.
 	 * @return 	void
 	 **/
 	private function parse_json( $json ){
 
 		if( !$json ){ return false; }
+
 		$x = json_decode($json, true);
 		$x = $x['zypcreds'];
 
@@ -367,16 +373,17 @@ class ZypCredsREST{
 
 	
 	/**
-	 * create_hash( $data )
+	 * create_hash()
 	 * Summary: Creates a hash value from the params passed to it.
-	 * @param 	arr 	$data 	Parameters from user.
+	 * @param 	arr 	$params Parameters being passed to API.
 	 * @return 	str 	$hash 	The properly hashed value from using params and the api_key
 	 **/
-	private function create_hash( $data ){
-
+	private function create_hash( $params ){
 		$str = '';
-		foreach( $data as $k => $v ){
-			if($k !='hash' && $k !='siteurl'){ $str .= $k.$v; }
+		foreach( $params as $k => $v ){
+			if($k !='hash' && $k !='siteurl'){
+				$str .= $k.$v;
+			}
 		}
 		return hash_hmac('sha1', $str, $this->api_key);
 	}
